02219c6927db874c3ed1352cf08fe5748060cce7
Zhuo <hudirybw@gmail.com>
2022-11-03 13:16:06 +0100

    feat: fix redirect url

--- server/controllers/githubAndGoogleAuth.js
+++ server/controllers/githubAndGoogleAuth.js
@@ -2,11 +2,11 @@ const config = require('../../config')
 
 function githubCallback (req, res) {
     req.session.user = req.user
-    res.redirect(config[config.model].domain + '/profile');
+    res.redirect(config[config.model].frontEnd + '/profile');
 }
 
 function googleCallback (req, res) {
     req.session.user = req.user
-    res.redirect(config[config.model].domain + '/profile');
+    res.redirect(config[config.model].frontEnd + '/profile');
 }
 module.exports = { githubCallback, googleCallback }
