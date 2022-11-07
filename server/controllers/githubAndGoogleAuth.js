d2500eb3f2e1362d4322360da42567c981d82de0
FrankoSaf <franko.safradin@gmail.com>
2022-11-07 17:31:02 +0100

    yes

--- server/controllers/githubAndGoogleAuth.js
+++ server/controllers/githubAndGoogleAuth.js
@@ -1,12 +1,12 @@
-const config = require('../../config')
+const config = require('../config');
 
-function githubCallback (req, res) {
-    req.session.user = req.user
-    res.redirect(config[config.model].frontEnd + '/profile');
+function githubCallback(req, res) {
+  req.session.user = req.user;
+  res.redirect(config[config.model].frontEnd + '/profile');
 }
 
-function googleCallback (req, res) {
-    req.session.user = req.user
-    res.redirect(config[config.model].frontEnd + '/profile');
+function googleCallback(req, res) {
+  req.session.user = req.user;
+  res.redirect(config[config.model].frontEnd + '/profile');
 }
-module.exports = { githubCallback, googleCallback }
+module.exports = { githubCallback, googleCallback };
