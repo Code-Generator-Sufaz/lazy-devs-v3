1a35809b24a0ef6fd610f6b815e22e5cf8d813c9
urieskenasy <urieskenasy@gmail.com>
2022-11-09 10:08:24 +0100

    deploy commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,13 +1,12 @@
 const config = {
-
   model: "deploy", // "deploy"
   dev: {
-    domain: 'http://localhost:5000',
-    frontEnd: 'http://localhost:3000',
+    domain: "http://localhost:5000",
+    frontEnd: "http://localhost:3000",
   },
   deploy: {
-    domain: 'https://lazydevs.onrender.com',
-    frontEnd: 'https://lazydevs.onrender.com',
+    domain: "https://lazydevs.onrender.com",
+    frontEnd: "https://lazydevs.onrender.com",
   },
 };
 // "proxy": "http://localhost:5000",
